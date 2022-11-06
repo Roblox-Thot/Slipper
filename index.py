fb733c4ce99165141e20add3265e2ba4db7c7255
RobloxThot <67937010+Roblox-Thot@users.noreply.github.com>
2022-11-06 17:48:47 -0500

    Do some shit ig lol

--- index.py
+++ index.py
@@ -1,6 +1,5 @@
-
 from PIL import Image
-import random, os
+import random, os, math
 
 folder = "out"
 
@@ -31,9 +30,12 @@ asciiName = """ ::::::::  :::        ::::::::::: :::::::::  :::::::::  :::::::::
  ########  ########## ########### ###        ###        ########## ###    ###
                              Roblox image slipper\n"""
 
-def menu(a):
+def printName():
     os.system('cls' if os.name == 'nt' else 'clear')
     print(asciiName)
+
+def menu(a):
+    printName()
     return input(f'{a}: ')
 #endregion
 
@@ -45,11 +47,18 @@ if menu("Resize? (y/n)") == "y":
     size = menu("Size (x,y): ").split(",")
     img = img.resize((int(size[0]),int(size[1])))
 
+def progressBar(msg,number,outOf):
+    count = math.ceil(((number+1)/outOf)*30)
+    present = math.ceil(((number+1)/outOf)*100)
+    print(f'{msg} (O{"o"*count}f{"_"*(30-count)}) {present}% ', end='\r')
+
 #region methods
 # Make images with alphas from 150 to 255
 def alphasMethod():
+    printName()
     count = 0
-    for a in range(150,256):
+    for a in range(155,255):
+        progressBar("Making images", count, 100)
         count += 1
         rgba = img.convert("RGBA")
         datas = rgba.getdata()
@@ -59,11 +68,14 @@ def alphasMethod():
             newData.append((item[0], item[1], item[2], a))
 
         rgba.putdata(newData)
-        rgba.save(f'{folder}/{a}.png', "PNG")
+        rgba.save(f'{folder}/{count}.png', "PNG")
+    print()
 
 # Random color on a random pixel
-def randomColorMethod(count,intensity):
+def randomColorMethod(count):
+    printName()
     for a in range(0,count):
+        progressBar("Making images", a, count)
         rgba = img.convert("RGBA")
         datas = rgba.getdata()
 
@@ -76,16 +88,18 @@ def randomColorMethod(count,intensity):
         ran = random.randint(0,int(len(newData)))
         # Sets the color
         newData[ran]=(
-            random.randint(0,intensity),
-            random.randint(0,intensity),
-            random.randint(0,intensity), 0)
-        print(ran,newData[ran])
+            random.randint(0,item[0]),
+            random.randint(0,item[1]),
+            random.randint(0,item[2]), item[3])
         rgba.putdata(newData)
         rgba.save(f'{folder}/{a+1}.png', "PNG")
+    print()
 
 # Add static to image
 def staticMethod(count,intensity):
+    printName()
     for a in range(0,count):
+        progressBar("Making images", a, count)
         rgba = img.convert("RGBA")
         datas = rgba.getdata()
         newData = []
@@ -95,16 +109,16 @@ def staticMethod(count,intensity):
                 item[0] + random.randint(0,intensity),
                 item[1] + random.randint(0,intensity),
                 item[2] + random.randint(0,intensity), item[3]))
-        print(a)
         rgba.putdata(newData)
         rgba.save(f'{folder}/{a+1}.png', "PNG")
+    print()
 #endregion
 
-choice = menu("Choose method\n\n1. Alphas\n2. Random color\n3. Static\n\nChoice")
+choice = menu("Choose method\n\n1. Alpha (change alpha from 150-255)\n2. Random color (sets a random pixel to a random color)\n3. Static (adds a static to the image)\n\nChoice")
 if choice == "1":
     alphasMethod()
 elif choice == "2":
-    randomColorMethod(int(menu("How many images?")),int(menu("How much intensity? (1-255)")))
+    randomColorMethod(int(menu("How many images?")))
 elif choice == "3":
     staticMethod(int(menu("How many images?")),int(menu("How much intensity? (1-255)")))
 else:
