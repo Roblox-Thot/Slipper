92020e6ea67091a6d09feb16d0359f6ef06570a3
RobloxThot <67937010+Roblox-Thot@users.noreply.github.com>
2022-11-06 17:11:15 -0500

    ignore tests

--- index.py
+++ index.py
@@ -2,8 +2,6 @@
 from PIL import Image
 import random, os
 
-file = input("Image file: ").replace("\"", "")
-#file = "320.png"
 folder = "out"
 
 # checking if the directory 
@@ -23,51 +21,91 @@ else:
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
 
+#region menu
+asciiName = """ ::::::::  :::        ::::::::::: :::::::::  :::::::::  :::::::::: :::::::::
+:+:    :+: :+:            :+:     :+:    :+: :+:    :+: :+:        :+:    :+:
++:+        +:+            +:+     +:+    +:+ +:+    +:+ +:+        +:+    +:+
++#++:++#++ +#+            +#+     +#++:++#+  +#++:++#+  +#++:++#   +#++:++#:
+       +#+ +#+            +#+     +#+        +#+        +#+        +#+    +#+
+#+#    #+# #+#            #+#     #+#        #+#        #+#        #+#    #+#
+ ########  ########## ########### ###        ###        ########## ###    ###
+                             Roblox image slipper\n"""
+
+def menu(a):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(asciiName)
+    return input(f'{a}: ')
+#endregion
+
+file = menu("Image file").replace("\"", "")
+
 img = Image.open(file)
-#img = img.resize((450,450))
 
+if menu("Resize? (y/n)") == "y":
+    size = menu("Size (x,y): ").split(",")
+    img = img.resize((int(size[0]),int(size[1])))
+
+#region methods
 # Make images with alphas from 150 to 255
-# for a in range(150,256):
-#     rgba = img.convert("RGBA")
-#     datas = rgba.getdata()
-    
-#     newData = []
-#     for item in datas:
-#         newData.append((item[0], item[1], item[2], a))
-    
-#     rgba.putdata(newData)
-#     rgba.save(f'{folder}/{a}.png', "PNG")
+def alphasMethod():
+    count = 0
+    for a in range(150,256):
+        count += 1
+        rgba = img.convert("RGBA")
+        datas = rgba.getdata()
 
+        newData = []
+        for item in datas:
+            newData.append((item[0], item[1], item[2], a))
+
+        rgba.putdata(newData)
+        rgba.save(f'{folder}/{a}.png', "PNG")
 
 # Random color on a random pixel
-# for a in range(1,51):
-#     rgba = img.convert("RGBA")
-#     datas = rgba.getdata()
-#
-#     newData = []
-#
-#     for item in datas:
-#        newData.append(item)
-#
-#     # Picks random pixel to replace
-#     ran = random.randint(0,int(len(newData)))
-#     # Sets the color
-#     newData[ran]=(random.randint(0,255), random.randint(0,255), random.randint(0,255), 0)
-#     print(ran,newData[ran])
-#     rgba.putdata(newData)
-#     rgba.save(f'{folder}/{a}.png', "PNG")
+def randomColorMethod(count,intensity):
+    for a in range(0,count):
+        rgba = img.convert("RGBA")
+        datas = rgba.getdata()
+
+        newData = []
+
+        for item in datas:
+            newData.append(item)
+
+        # Picks random pixel to replace
+        ran = random.randint(0,int(len(newData)))
+        # Sets the color
+        newData[ran]=(
+            random.randint(0,intensity),
+            random.randint(0,intensity),
+            random.randint(0,intensity), 0)
+        print(ran,newData[ran])
+        rgba.putdata(newData)
+        rgba.save(f'{folder}/{a+1}.png', "PNG")
 
 # Add static to image
-for a in range(1,31):
-    rgba = img.convert("RGBA")
-    datas = rgba.getdata()
-    newData = []
+def staticMethod(count,intensity):
+    for a in range(0,count):
+        rgba = img.convert("RGBA")
+        datas = rgba.getdata()
+        newData = []
 
-    for item in datas:
-       newData.append((
-        item[0] + random.randint(0,20),
-        item[1] + random.randint(0,20),
-        item[2] + random.randint(0,20), item[3]))
-    print(a)
-    rgba.putdata(newData)
-    rgba.save(f'{folder}/{a}.png', "PNG")
+        for item in datas:
+            newData.append((
+                item[0] + random.randint(0,intensity),
+                item[1] + random.randint(0,intensity),
+                item[2] + random.randint(0,intensity), item[3]))
+        print(a)
+        rgba.putdata(newData)
+        rgba.save(f'{folder}/{a+1}.png', "PNG")
+#endregion
+
+choice = menu("Choose method\n\n1. Alphas\n2. Random color\n3. Static\n\nChoice")
+if choice == "1":
+    alphasMethod()
+elif choice == "2":
+    randomColorMethod(int(menu("How many images?")),int(menu("How much intensity? (1-255)")))
+elif choice == "3":
+    staticMethod(int(menu("How many images?")),int(menu("How much intensity? (1-255)")))
+else:
+    print("Invalid choice")
